c97710e3a8f8d30d3e8198d81630fde2071fe9f1
luna <luna@wvvw.me>
2024-12-21 09:08:05 +1030

    chore: add conversation hooks

---
+++ src/lib/bluesky/hooks/useConversation.ts
@@ -0,0 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
+import { useBlueskyStore } from '../store';
+import { useAuth } from './useAuth';
+
+export function useConversation({ convoId }: { convoId: string }) {
+  const { agent, session } = useBlueskyStore();
+  const { isAuthenticated } = useAuth();
+
+  return useQuery({
+    queryKey: ['conversations', { convoId }],
+    queryFn: async () => {
+      if (!session) return [];
+
+      // @ts-expect-error bsky_chat does in fact work
+      const proxy = agent?.withProxy('bsky_chat', 'did:web:api.bsky.chat');
+      const response = await proxy?.api.chat.bsky.convo.getMessages({
+        convoId,
+      });
+
+      return response?.data.messages
+        .slice(0)
+        .sort((a, b) => new Date(a.sentAt as string).getTime() - new Date(b.sentAt as string).getTime());
+    },
+    enabled: !!agent && isAuthenticated,
+  });
+}
