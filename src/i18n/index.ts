1fa45f64f6e051e3301cb0af098655f93f12910a
Charles Dong <chardon_cs@proton.me>
2024-12-20 09:56:29 +0800

    feat: added `ja` (Japanese), `yue` (Cantonese), `zh-hans` (Simplified Chinese), and `zh-hant` (Traditional Chinese) (#5)

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -3,15 +3,23 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import Pseudo from 'i18next-pseudo';
 import { initReactI18next } from 'react-i18next';
 import { en } from './lang/en';
+import { ja } from './lang/ja';
 import { nl } from './lang/nl';
 import { ur } from './lang/ur';
+import { yue } from './lang/yue';
+import { zh_Hans } from './lang/zh-Hans';
+import { zh_Hant } from './lang/zh-Hant';
 
 export const defaultNS = 'app';
 
 export const languages = {
   en,
+  ja,
   nl,
   ur,
+  yue,
+  "zh-Hans": zh_Hans,
+  "zh-Hant": zh_Hant,
 };
 
 i18n
