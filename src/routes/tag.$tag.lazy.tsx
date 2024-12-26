08f9c3140939d8fe6248d45156fcc8edffd33701
luna <luna@wvvw.me>
2024-12-27 09:24:57 +1030

    fix: add canonical link for tag page

--- src/routes/tag.$tag.lazy.tsx
+++ src/routes/tag.$tag.lazy.tsx
@@ -1,6 +1,7 @@
 import { createLazyFileRoute } from '@tanstack/react-router';
 import { PostCard } from '../components/PostCard';
 import { useTag } from '../lib/bluesky/hooks/useTag';
+import { Helmet } from 'react-helmet';
 
 export const Route = createLazyFileRoute('/tag/$tag')({
   component: Tag,
@@ -10,5 +11,12 @@ function Tag() {
   const { tag } = Route.useParams();
   const { data: posts } = useTag({ tag: tag });
 
-  return posts?.map((post) => <PostCard key={post.uri} post={post} />);
+  return (
+    <>
+      <Helmet>
+        <link rel="canonical" href={`https://bsky.app/hashtag/${tag}`} />
+      </Helmet>
+      {posts?.map((post) => <PostCard key={post.uri} post={post} />)}
+    </>
+  );
 }
