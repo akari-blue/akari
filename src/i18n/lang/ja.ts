492d3f6a64dec00051e932542c95bfce443a3a28
Charles Dong <chardon_cs@proton.me>
2024-12-28 14:55:05 +0800

    chore: update Japanese translations (#23)

--- src/i18n/lang/ja.ts
+++ src/i18n/lang/ja.ts
@@ -7,10 +7,12 @@ export const ja = {
     },
     logout: 'サインアウト',
     password: 'パスワード',
+    authFactorToken: '二要素トークン',
   },
   app: {
     appName: '[placeholder name]',
     loading: 'ロード中...',
+    loadMore: 'さらにロードする',
     blueskyHandle: 'blueskyハンドル　例：@alice.bsky.social',
     following: 'フォロー中',
     followers: 'フォロワー',
@@ -19,6 +21,9 @@ export const ja = {
     reposts: 'リポスト',
     likes: 'いいね',
     settings: '設定',
+    notFound: '見つかりません',
+    save: '保存',
+    optional: '任意',
   },
   settings: {
     developerMode: {
@@ -66,9 +71,20 @@ export const ja = {
   profile: {
     profile: 'プロフィール',
     notFound: 'プロフィールが見つかりません',
+    tabs: {
+      all: 'すべて',
+      posts: '投稿',
+      media: 'メディア',
+      feeds: 'フィード',
+      reposts: 'リポスト',
+      likes: 'いいね',
+      lists: 'リスト',
+      starterpacks: 'スターターパック',
+    },
   },
   post: {
     notFound: '投稿が見つかりません',
+    createPost: '投稿を作成',
     blockedAuthor: '投稿者をブロックしているため、この投稿は非表示になっています。',
     blockedByAuthor: '投稿者があなたをブロックしているため、この投稿は非表示になっています。',
   },
@@ -78,5 +94,43 @@ export const ja = {
   },
   handleSearch: {
     noResultsFound: '結果が見つかりません',
+  },
+  notifications: {
+    notifications: '通知',
+    noNotifications: '通知なし',
+    followedYou: 'あなたをフォローしました',
+    likedYourPost: 'あなたの投稿をいいねしました',
+    repostedYourPost: 'あなたの投稿をリポストしました',
+    repliedToYourPost: 'あなたの投稿に返信しました',
+    mentionedYou: 'あなたについてメンションしました',
+    quotedYourPost: 'あなたの投稿を引用しました',
+    joinedYourStarterpack: 'あなたのスターターパックに参加しました',
+    tabs: {
+      all: 'すべて',
+      mentions: 'メンション',
+      grouped: 'グループされた',
+    },
+  },
+  messages: {
+    messages: 'メッセージ',
+    noMessages: 'メッセージなし',
+  },
+  dialog: {
+    close: '閉じる',
+  },
+  editor: {
+    toolbar: {
+      link: {
+        edit: 'リンクを編集',
+        openInNewTab: '新しいタブで開く',
+        displayText: 'テキストを表示',
+      },
+      image: {
+        title: '画像をアップロードする',
+        description: 'コンピュータから画像をアップロードする',
+        failedToLoad: '画像のロードに失敗しました',
+        submit: 'サブミット',
+      },
+    },
   },
 } as const;
