95dab1023f14aaaaaf3cdfaca5a78b6c7ad20fb3
luna <luna@wvvw.me>
2024-12-28 11:53:20 +1030

    chore: split bundle into vendor chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
@@ -5,7 +6,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react({})],
+  plugins: [TanStackRouterVite(), react({}), visualizer()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -13,5 +14,21 @@ export default defineConfig({
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('graphemer')) return 'graphemer';
+          if (id.includes('@atproto')) return '@atproto';
+          if (id.includes('@tiptap')) return '@tiptap';
+          if (id.includes('@stdlib')) return '@stdlib';
+          if (id.includes('@tanstack/query-devtools')) return '@tanstack/query-devtools';
+          if (id.includes('@tanstack')) return '@tanstack';
+          if (id.includes('prosemirror')) return 'prosemirror';
+          if (id.includes('node_modules')) return 'vendor';
+        },
+      },
+    },
   },
 });
