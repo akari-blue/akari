b9d9f6125048baad005ebcf7948e7cc8b006c14f
luna <luna@wvvw.me>
2025-01-02 13:14:13 +1030

    feat: prefix and intl support for formatted numbers

--- src/components/ui/FormattedNumber.test.tsx
+++ src/components/ui/FormattedNumber.test.tsx
@@ -5,6 +5,34 @@ import { FormattedNumber } from './FormattedNumber';
 describe('FormattedNumber', () => {
   test('renders', () => {
     const { container } = render(<FormattedNumber value={1000} unit="followers" />);
-    expect(container).not.toBeEmptyDOMElement();
+    expect(container).toHaveTextContent('1K followers');
+  });
+
+  test('renders with a prefix', () => {
+    const { container } = render(<FormattedNumber value={1000} unit="followers" prefix="+" />);
+    expect(container).toHaveTextContent('+1K followers');
+  });
+
+  test('renders in compact notation', () => {
+    const { container } = render(<FormattedNumber value={1000} unit="followers" prefix="+" notation="compact" />);
+    expect(container).toHaveTextContent('+1K followers');
+  });
+
+  test('renders in standard notation', () => {
+    const { container } = render(<FormattedNumber value={1000} unit="followers" prefix="+" notation="standard" />);
+    expect(container).toHaveTextContent('+1,000 followers');
+  });
+
+  test('renders nothing when value is undefined', () => {
+    const { container } = render(<FormattedNumber value={undefined} unit="followers" />);
+    expect(container).toBeEmptyDOMElement();
+  });
+
+  test('renders in a different language', () => {
+    Object.defineProperty(navigator, 'languages', {
+      value: ['ar-SA'],
+    });
+    const { container } = render(<FormattedNumber value={1000} unit="followers" />);
+    expect(container).toHaveTextContent('١ ألف followers');
   });
 });
