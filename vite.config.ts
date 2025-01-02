11899a6e786422fdfdceb4f27929acc315e0b906
luna <luna@wvvw.me>
2025-01-03 09:49:48 +1030

    chore: add og tags

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import { VitePWA } from 'vite-plugin-pwa';
+import ogPlugin from 'vite-plugin-open-graph';
+import { name, description } from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,9 +21,9 @@ export default defineConfig({
       includeAssets: ['src/assets/images/**/*'],
       manifest: {
         id: 'blue.akari',
-        name: 'akari',
-        short_name: 'akari' + (process.env.NODE_ENV === 'development' ? ' (Dev)' : ''),
-        description: 'akari',
+        name,
+        short_name: name + (process.env.NODE_ENV === 'development' ? ' (Dev)' : ''),
+        description,
         theme_color: '#000000',
         icons: [
           {
@@ -47,6 +49,12 @@ export default defineConfig({
             form_factor: 'narrow',
           },
         ],
+      },
+    }),
+    ogPlugin({
+      basic: {
+        siteName: name,
+        description,
       },
     }),
   ],
