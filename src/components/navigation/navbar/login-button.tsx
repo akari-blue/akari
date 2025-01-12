7b08e2beffe9c360bcaf46eba7159c3b8d0ad8ee
luna <luna@wvvw.me>
2025-01-12 20:59:45 +1030

    fix: missing login button

--- src/components/navigation/navbar/login-button.tsx
+++ src/components/navigation/navbar/login-button.tsx
@@ -4,12 +4,13 @@ import { LogInIcon } from 'lucide-react';
 
 export const LoginButton = () => {
   const { t } = useTranslation('auth');
+
   return (
     <Link
       to="/login"
-      className="flex-row items-center gap-2 p-3 rounded-sm hover:no-underline hover:bg-gray-200 dark:hover:bg-gray-700 hidden justify-center md:flex"
+      className="flex flex-row gap-2 p-3 rounded-sm hover:no-underline hover:bg-gray-200 dark:hover:bg-gray-700 justify-center items-center"
     >
-      <LogInIcon className="size-6 xl:hidden active:scale-90" />
+      <LogInIcon className="size-6 active:scale-90" />
       <span className="hidden xl:block">{t('login.default')}</span>
     </Link>
   );
